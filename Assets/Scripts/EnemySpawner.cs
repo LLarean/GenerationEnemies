340342bc446c334a146e4675826fbb3b2a02d858
Stanislav Kozlov <llarean@ya.ru>
2023-03-30 11:36:28 +0300

    Changing positions, changing the spawn code

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,47 +1,54 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _enemy;
     [SerializeField] private List<Transform> _spawnPositions;
-    [SerializeField] private float _spawnTime;
+    [SerializeField] private float _spawnDelay;
     [SerializeField] private int _maximumNumberEnemies;
 
     private List<GameObject> _enemies = new List<GameObject>();
     private float _runningTime;
-    private int _spawnCount = 0;
+    private int _spawnCount;
+    private int _defaultSpawnCount = 0;
     
     private void Update()
     {
         _runningTime += Time.deltaTime;
-
-        if (_runningTime >= _spawnTime)
+        
+        if (_runningTime >= _spawnDelay)
         {
-            RemoveUnnecessaryEnemies();
-            
-            var position = _spawnPositions[_spawnCount].transform.position;
-            _spawnCount++;
-
-            if (_spawnCount == _spawnPositions.Count)
-            {
-                _spawnCount = 0;
-            }
-            
-            GameObject enemy = Instantiate(_enemy, position, Quaternion.identity);
-            _enemies.Add(enemy);
+            SpawnEnemy();
             _runningTime = 0;
         }
+    }
+
+    private void SpawnEnemy()
+    {
+        RemoveUnnecessaryEnemies();
+
+        var spawnPosition = _spawnPositions[_spawnCount].transform.position;
+        _spawnCount++;
+
+        if (_spawnCount == _spawnPositions.Count)
+        {
+            _spawnCount = _defaultSpawnCount;
+        }
+
+        GameObject enemy = Instantiate(_enemy, spawnPosition, Quaternion.identity);
+        _enemies.Add(enemy);
     }
 
     private void RemoveUnnecessaryEnemies()
     {
         if (_enemies.Count >= _maximumNumberEnemies)
         {
-            var enemyForDeliting = _enemies[0];
-            _enemies.Remove(enemyForDeliting);
-            Destroy(_enemies[0].gameObject);
+            int firstEnemyCount = 0;
+            var enemyToDelete = _enemies[firstEnemyCount];
+            
+            _enemies.Remove(enemyToDelete);
+            Destroy(enemyToDelete);
         }
     }
 }
